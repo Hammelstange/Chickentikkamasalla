a23883074dde388c7f99bf374a72125f414dd500
Hammelstange <heinichtoni@gmail.com>
2024-12-10 19:07:04 +0100

    data uploeded

---
+++ chickencode.js
@@ -0,0 +1,6 @@
+function ingredients() {
+    const Ingredients = "Garam masala, Cumin, Rote Zwiebeln, Ingwer, Joghurt"
+    const newWindow = window.open();
+    newWindow.document.write(Ingredients)
+}
+
